2cb95d1a15a866fa191c11e308ad6e2f0c65ead5
zhangxiaodong <774793337@qq.com>
2019-06-21 14:57:50 +0800

    Update date-picker.vue

--- src/components/DatePicker/date-picker.vue
+++ src/components/DatePicker/date-picker.vue
@@ -1,21 +1,34 @@
 <template>
-  <div v-click-outside>
+  <div v-click-outside class="date-picker">
     <input
       type="text"
       class="input"
       :value="formatDate"
       :placeholder="placeholderText"
-      @focus="handleFocus"
-      @blur="handleBlur"
     >
-    <div v-if="isVisible" class="pannel">
-      <div>d</div>
-    </div>
+    <transition name="fade">
+      <div v-if="isVisible" class="pannel">
+        <div class="pannel-nav">
+          <span>&lt;</span>
+          <span>&lt;&lt;</span>
+          <span>xxxx年</span>
+          <span>xx月</span>
+          <span>&gt;</span>
+          <span>&gt;&gt;</span>
+        </div>
+        <div class="pannel-concent">
+          内容
+        </div>
+        <div class="pannel-footer">
+          今天
+        </div>
+      </div>
+    </transition>
   </div>
 </template>
 
 <script>
-import * as utlis from './util.js'
+import * as utlis from './util'
 
 export default {
   name: 'date-paicker',
@@ -25,21 +38,38 @@ export default {
       default: () => new Date()
     }
   },
-  directives: {
-    clickOutSide: {
-      bind () {
-        
-      }
-    }
-  },
   data () {
     return {
       placeholderText: '请选择日期',
       isVisible: false // 日期面板显隐
     }
   },
+  directives: {
+    clickOutside: {
+      bind (el, binding, vnode) { // vnode 当前context上下文的虚拟节点
+        let handler = (e) => {
+          // 判断当前目标是否包含在该dom元素内
+          if (el.contains(e.target)) {
+            // 判断当前面板是否显示
+            if (!vnode.context.isVisible) {
+              vnode.context.handleFocus()
+            }
+          } else {
+            if (vnode.context.isVisible) {
+              vnode.context.handleBlur()
+            }
+          }
+        }
+        el.handler = handler
+        document.addEventListener('click', handler)
+      },
+      unbind (el) {
+        document.removeEventListener('click', el.handler)
+      }
+    }
+  },
   computed: {
-    formatDate () {
+    formatDate () { // 格式化日期 yyyy-mm-dd
       let { year, month, day } = utlis.getFormatDate(this.value)
       return `${year}-${month}-${day}`
     }
@@ -56,28 +86,45 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  .input {
-    display: inline-block;
+  .date-picker {
     width: 180px;
-    height: 30px;
-    line-height: 30px;
-    padding: 0 8px;
-    color: #333;
-    font-size: 12px;
-    border: 1px solid #ddd;
-    border-radius: 2px;
-    cursor: text;
-    transition: border .2s ease-in-out, background .2s ease-in-out, box-shadow .2s ease-in-out;
-      &:hover {
-        border: 1px solid #1890ff;
-      }
-      &:focus {
-        border-color: #1890ff;
-        outline: none;
-        box-shadow: none;
-      }
-  }
-  .pannel {
 
+    .input {
+      display: inline-block;
+      width: 100%;
+      height: 30px;
+      line-height: 30px;
+      padding: 0 8px;
+      color: #333;
+      font-size: 12px;
+      border: 1px solid #ddd;
+      border-radius: 2px;
+      cursor: text;
+      transition: border .2s ease-in-out, background .2s ease-in-out, box-shadow .2s ease-in-out;
+        &:hover {
+          border: 1px solid #1890ff;
+        }
+        &:focus {
+          border-color: #1890ff;
+          outline: none;
+          box-shadow: none;
+        }
+    }
+  }
+
+  .fade-enter {
+    opacity: 0;
+  }
+
+  .fade-enter-active {
+    transition: opacity 1s;
+  }
+
+  .fade-leave-to {
+    opacity: 0;
+  }
+
+  .fade-leave-active {
+    transition: opacity 1s;
   }
 </style>
